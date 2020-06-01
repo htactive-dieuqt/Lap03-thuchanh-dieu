71fbe137e4a18f7883281051b374f1fa246636c6
htactive-dieuqt <dieuqt@htactive.com>
2020-06-01 17:46:46 +0700

    responsive

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 4000);
 }
+
+function showInformation() {
+    const name = document.getElementById("name").value;
+    const mail = document.getElementById("mail").value;
+    var text = ""
+    const subject = document.getElementById("subject").value;
+    const mesage = document.getElementById("mesage").value
+    if (name === "" || mail === "") {
+        text = "Input not valid";
+    } else {
+        console.log(name, mail, '????');
+
+        document.getElementById("name1").innerHTML = name;
+        document.getElementById("mail2").innerHTML = mail;
+        document.getElementById("subject3").innerHTML = subject;
+        document.getElementById("mesage4").innerHTML = mesage;
+    }
+    document.getElementById("notvalid").innerHTML = text
+}
